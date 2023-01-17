420c2014ff428a8cb254e7a2704063d5b7ed2832
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2023-01-17 19:32:19 -0300

    :rocket: configurando o driver do google chrome

---
+++ selenium-java-projeto/src/main/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -0,0 +1,21 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldSelenium {
+
+    @Test
+    public void hello() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        WebDriver browser = new ChromeDriver();
+
+        browser.navigate().to("http://localhost:8080/login");
+
+        browser.quit();
+
+    }
+}
+//"C:\Users\crist\Documents\Workspace\Cursos\Alura\Trilha-QA-Alura\selenium-java-projeto\src\drivers\chromedriver.exe"
